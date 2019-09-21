2150b3eeacd2dc187138c3ce63849d1ad5519bb3
Leonardo Souza <lsouzafapos@gmail.com>
2019-09-21 16:58:43 -0300

    Auditing data: PreUpdate & PrePersist

--- src/main/java/com/educandoweb/course/entities/Category.java
+++ src/main/java/com/educandoweb/course/entities/Category.java
@@ -1,6 +1,7 @@
 package com.educandoweb.course.entities;
 
 import java.io.Serializable;
+import java.time.Instant;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -9,8 +10,9 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToMany;
+import javax.persistence.PrePersist;
+import javax.persistence.PreUpdate;
 import javax.persistence.Table;
-
 
 @Entity
 @Table(name = "tb_category")
@@ -22,9 +24,11 @@ public class Category implements Serializable {
 	private Long id;
 	private String nome;
 
-	
 	@ManyToMany(mappedBy = "categories")
 	private Set<Product> products = new HashSet<>();
+
+	private Instant createdAt;
+	private Instant updatedAt;
 
 	public Category() {
 
@@ -51,9 +55,29 @@ public class Category implements Serializable {
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
-	
+
 	public Set<Product> getProducts() {
 		return products;
+	}
+
+	public Instant getCreatedAt() {
+		return createdAt;
+	}
+
+	public Instant getUpdatedAt() {
+		return updatedAt;
+	}
+
+	@PreUpdate
+	public void preUpdate() {
+		updatedAt = Instant.now();
+	}
+
+	@PrePersist
+	public void prePersist() {
+		Instant now = Instant.now();
+		updatedAt = now;
+		createdAt = now;
 	}
 
 	@Override
