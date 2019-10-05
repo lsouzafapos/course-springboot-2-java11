9d77b8d0e3bf56efb096fa02e289f9e18727a67f
Leonardo Souza <lsouzafapos@gmail.com>
2019-10-05 16:16:06 -0300

    Authorization filter

--- src/main/java/com/educandoweb/course/config/SecurityConfig.java
+++ src/main/java/com/educandoweb/course/config/SecurityConfig.java
@@ -13,16 +13,26 @@ import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import com.educandoweb.course.security.JWTAuthorizationFilter;
+import com.educandoweb.course.security.JWTUtil;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	@Autowired
 	private Environment env;
+
+	@Autowired
+	private JWTUtil jwtUtil;
+
+	@Autowired
+	private UserDetailsService userDetailsService;
 
 	private static final String[] PUBLIC_MATCHERS_GET = { "/products/**", "/categories/**" };
 
@@ -44,6 +54,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		http.authorizeRequests().antMatchers(HttpMethod.POST, PUBLIC_MATCHERS_POST).permitAll();
 		http.authorizeRequests().antMatchers(PUBLIC_MATCHERS).permitAll();
 		http.authorizeRequests().anyRequest().authenticated();
+		
+		http.addFilter(new JWTAuthorizationFilter(authenticationManager(), jwtUtil, userDetailsService));
 	}
 
 	// SWAGGER
