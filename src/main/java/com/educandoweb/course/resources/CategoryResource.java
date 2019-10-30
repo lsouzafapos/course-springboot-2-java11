70a5b61b17527ceddf9e4e9acb3c3b7b7061b651
Leonardo Souza <lsouzafapos@gmail.com>
2019-10-30 14:55:20 -0300

    Category endpoints

--- src/main/java/com/educandoweb/course/resources/CategoryResource.java
+++ src/main/java/com/educandoweb/course/resources/CategoryResource.java
@@ -33,6 +33,14 @@ public class CategoryResource {
 		return ResponseEntity.ok().body(list);
 
 	}
+	
+	@GetMapping(value = "/product/{productId}")
+	public ResponseEntity<List<CategoryDTO>> findByProduct(@PathVariable Long productId) {
+
+		List<CategoryDTO> list = service.findByProduct(productId);
+		return ResponseEntity.ok().body(list);
+
+	}
 
 	@GetMapping(value = "/{id}")
 	public ResponseEntity<CategoryDTO> findById(@PathVariable Long id) {
