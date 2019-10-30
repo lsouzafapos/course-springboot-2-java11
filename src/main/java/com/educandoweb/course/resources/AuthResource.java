370dbfca9d471d6440e0b8d5e5ddf2fd7d6e1735
Leonardo Souza <lsouzafapos@gmail.com>
2019-10-30 13:59:18 -0300

    Refresh token

--- src/main/java/com/educandoweb/course/resources/AuthResource.java
+++ src/main/java/com/educandoweb/course/resources/AuthResource.java
@@ -27,5 +27,11 @@ public class AuthResource {
 		TokenDTO tokenDTO = service.authenticate(dto);
 		return ResponseEntity.ok().body(tokenDTO);
 	}
+	
+	@PostMapping("/refresh")
+	public ResponseEntity<TokenDTO> refresh() {
+		TokenDTO tokenDTO = service.refreshToken();
+		return ResponseEntity.ok().body(tokenDTO);
+	}
 
 }
